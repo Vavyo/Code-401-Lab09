97b6b38f6eb5c64c83103500099a48f5ee452c4a
Vavyo <vavyo2121@gmail.com>
2021-04-05 00:50:25 -0500

    FINALLY GOT IT TO PARSE

---
+++ LINQinManhattan/Classes/Point.cs
@@ -0,0 +1,8 @@
+﻿namespace LINQinManhattan.Classes
+{
+    public class Point
+    {
+        public string type { get; set; }
+        public decimal[] coordinates { get; set; }
+    }
+}
--- LINQinManhattan/Program.cs
+++ LINQinManhattan/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using LINQinManhattan.Classes;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace LINQinManhattan
 {
@@ -7,6 +11,19 @@ namespace LINQinManhattan
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            JsonTests();
+        }
+        public static void JsonTests()
+        {
+            
+            FeatureCollection collection = JsonConvert.DeserializeObject<FeatureCollection>(File.ReadAllText(@"E:\Delta V\401\Code-401-Lab09\data.json"));
+            var results =
+                from c in collection.features
+                select c.properties.neighborhood;
+            foreach(string i in results)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
